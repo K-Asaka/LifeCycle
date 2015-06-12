3557baef726b25981120ad34d98f67d199d2a1a4
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2015-06-12 18:00:01 +0900

    Add a fragment.
    Add a fragment, verify cycle. Yet still contain errors.

--- src/jp/ac/chibafjb/app/lifecycle/MainActivity.java
+++ src/jp/ac/chibafjb/app/lifecycle/MainActivity.java
@@ -1,10 +1,14 @@
 package jp.ac.chibafjb.app.lifecycle;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "LIFECYCLE";
@@ -75,5 +79,89 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	
+	public static class CycleFragment extends Fragment {
+		private static final String TAG = "Fragment";
+
+		@Override
+		public void onAttach(Activity activity) {
+			// TODO Auto-generated method stub
+			super.onAttach(activity);
+			Log.v(TAG, "onAttache()が呼ばれました。");
+		}
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+			Log.v(TAG, "onCreate()が呼ばれました。");
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			Log.v(TAG, "onCreateView()が呼ばれました。");
+			return super.onCreateView(inflater, container, savedInstanceState);
+		}
+		
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onActivityCreated(savedInstanceState);
+			Log.v(TAG, "onActivityCreated()が呼ばれました。");
+		}
+		
+		@Override
+		public void onStart() {
+			// TODO Auto-generated method stub
+			super.onStart();
+			Log.v(TAG, "onStart()が呼ばれました。");
+		}
+		
+		@Override
+		public void onResume() {
+			// TODO Auto-generated method stub
+			super.onResume();
+			Log.v(TAG, "onResume()が呼ばれました。");
+		}
+		
+		@Override
+		public void onPause() {
+			// TODO Auto-generated method stub
+			super.onPause();
+			Log.v(TAG, "onPause()が呼ばれました。");
+		}
+		
+		@Override
+		public void onStop() {
+			// TODO Auto-generated method stub
+			super.onStop();
+			Log.v(TAG, "onStop()が呼ばれました。");
+		}
+		
+		@Override
+		public void onDestroyView() {
+			// TODO Auto-generated method stub
+			super.onDestroyView();
+			Log.v(TAG, "onDestroyView()が呼ばれました。");
+		}
+		
+		@Override
+		public void onDestroy() {
+			// TODO Auto-generated method stub
+			super.onDestroy();
+			Log.v(TAG, "onDestroy()が呼ばれました。");
+		}
+		
+		@Override
+		public void onDetach() {
+			// TODO Auto-generated method stub
+			super.onDetach();
+			Log.v(TAG, "onDetach()が呼ばれました。");
+		}
 	}
 }
